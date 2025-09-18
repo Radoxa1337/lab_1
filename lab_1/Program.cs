ce046d4290d56fec6ae257444d02a8b613709317
radox <karwilaa@mail.ru>
2025-09-18 14:20:41 +0300

    Добавьте файлы проекта.

---
+++ lab_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1
+{
+    public class Challenge
+    {
+        public string subj = "Любой";
+        public string name = "Без названия";
+        public string date = "00.00.00";
+        public Challenge() { }
+        public Challenge(string subj, string name, string date)
+        {
+            this.subj = subj;
+            this.name = name;
+            this.date = date;
+        }
+    }
+
+    public class Exam:Challenge
+    {
+        private int pointsToPass = 0;
+        private string acceptKey = "none";
+        public Exam() { }
+        public Exam(string subj, string name, string date, int pointsToPass, string acceptKey):
+            base(subj, name, date)
+        {
+            this.pointsToPass = pointsToPass;
+            this.acceptKey = acceptKey;
+        }
+    }
+
+    public class EExam:Exam
+    {
+        private int pasportID = 1234;
+        private string indx = "xxxx-xxxx";
+        public EExam() { }
+        public EExam(string subj, string name, string date, int pointsToPass, string acceptKey, int pasportID, string indx)
+            : base(subj, name, date, pointsToPass, acceptKey)
+        { this.pasportID = pasportID; this.indx = indx;}      
+    }
+
+    public class Test : Challenge
+    {
+        public int maxPoints = 0;
+        public Test(string subj, string name, string date, int maxPoints)
+            :base(subj, name, date)
+        {
+            this.maxPoints = maxPoints;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
